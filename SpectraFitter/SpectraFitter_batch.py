62912a357887cfe2ffd3c05e357f724da1895a46
JornSteen <105309367+JornSteen@users.noreply.github.com>
2026-01-09 18:02:15 +0100

    Add Load button to SpectraFitter_batch
    New module called load_data.py in tools contains functions for loading data.

--- SpectraFitter/SpectraFitter_batch.py
+++ SpectraFitter/SpectraFitter_batch.py
@@ -1,5 +1,5 @@
-
 # -*- coding: utf-8 -*-
+import os
 import sys
 import numpy as np
 import pandas as pd
@@ -7,27 +7,28 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from PyQt5.QtWidgets import (QFileDialog,    
     QApplication, QWidget, QVBoxLayout, QHBoxLayout,
-    QPushButton, QLabel, QTextEdit, QLineEdit
+    QPushButton, QLabel, QTextEdit, QLineEdit, QMessageBox
 )
 from PyQt5.QtCore import Qt
 from SpectraFitter.tools.batch_FitPekarianGaussianHybrid import iterative_pekaria_fit
     ##!!! FIX importing of data in general module such that the same one can be used for both single and batch versions
 
+import SpectraFitter.tools.load_data as LoadData
+
 def main():
     class PekariaFitApp(QWidget):
-        def __init__(self, v_data, a_data):
+        def __init__(self):
             super().__init__()
-            self.v = v_data
-            self.a = a_data
             self.k_max = 10
             self.initUI()
-            self.full_transient_df = None
-            self.first_spectrum_loaded_from_transient = False
-    
         
         def initUI(self):
                 self.setWindowTitle("Manual Endpoint Fitting GUI")
                 layout = QVBoxLayout()
+
+                self.load_data_button = QPushButton("Load Spectra")
+                self.load_data_button.clicked.connect(self.load_full_dataset)
+                layout.addWidget(self.load_data_button)
     
                 layout.addWidget(QLabel("Centers (cm⁻¹), comma separated:"))
                 self.centers_text = QTextEdit()
@@ -71,12 +72,6 @@ def main():
                 self.store_last_button.clicked.connect(self.store_last_fit)
                 layout.addWidget(self.store_last_button)
     
-                
-    
-                
-    
-                
-    
                 self.fig_fit, self.ax_fit = plt.subplots(figsize=(8, 4))
                 self.canvas_fit = FigureCanvas(self.fig_fit)
                 layout.addWidget(self.canvas_fit)
@@ -89,9 +84,6 @@ def main():
                 self.output_console.setReadOnly(True)
                 self.output_console.setFixedHeight(100)
                 layout.addWidget(self.output_console)
-    
-                
-    
         
                 self.save_both_fits_button = QPushButton("Save First and Last Fits to CSV")
                 self.save_both_fits_button.clicked.connect(self.save_both_fits_to_csv)
@@ -101,20 +93,58 @@ def main():
                 self.fit_all_weights_button.clicked.connect(self.fit_all_spectra_with_weights)
                 layout.addWidget(self.fit_all_weights_button)
                 self.setLayout(layout)
+
+        def load_full_dataset(self):
+            self.load_file("Full Dataset")
+
+        def load_file(self, file_desc):
+            """Load a file based on the file description."""
+            try:
+                options = QFileDialog.Options()
+                ## File dialog for selecting files
+                file_name, _ = QFileDialog.getOpenFileName(self, f"Load {file_desc} File", "",
+                                                           "CSV, DAT Files (*.csv *dat);;DAT Files (*.dat);;All Files (*)", 
+                                                           options=options)
+                if not file_name:
+                    QMessageBox.warning(self, "Error", f"No {file_desc} file selected")
+                    return
     
-    
+                ##################################################
+                ## Store the file path in the appropriate attribute based on the file type/extension
+                file_ext = os.path.splitext(file_name)[1].lower()
+                
+                ## Load data dependent on file_ext
+                LoadData.loaded_data = LoadData.load_spectra(file_name, file_ext, file_desc)
+                
+                if file_desc == "Full Dataset":
+                    (LoadData.loaded_wavelengths, 
+                     LoadData.loaded_wavenumbers, 
+                     LoadData.loaded_spectra,
+                     LoadData.loaded_first_spectrum,
+                     LoadData.loaded_last_spectrum,
+                     LoadData.loaded_number_of_spectra) = LoadData.load_full(LoadData.loaded_data)
+                else:
+                    pass
+
+                QMessageBox.information(self, "Success", f"{file_desc} file loaded successfully!")
+            except Exception as e:
+                    QMessageBox.critical(self, "Error", f"Failed to load {file_desc} file: {e}")
+        
+        #####################################################################
+        #####################################################################
+
         def fit_first_spectrum(self):
-            if self.full_transient_df is None:
+            if LoadData.loaded_spectra is None:
                 self.output_console.append("⚠️ No transient data loaded.")
                 return
-            a_first = self.full_transient_df.iloc[:, 2].values
-            number_of_spectra = len(self.full_transient_df.iloc[0,2:])
-            
+            wavenumbers = LoadData.loaded_wavenumbers
+            a_first = LoadData.loaded_first_spectrum
+            number_of_spectra = LoadData.loaded_number_of_spectra
             
             centers = self.get_centers()
             k_max = int(self.kmax_input.text())
             try:
-                popt, _, v_fit, a_fit, comps = iterative_pekaria_fit(self.v, a_first, centers, k_max=k_max)
+                popt, _, v_fit, a_fit, comps = iterative_pekaria_fit(wavenumbers, a_first, centers, k_max=k_max)
                 fitted_centers = [popt[i * 6 + 2] for i in range(len(centers))]
                 # residuals = a_first - np.interp(self.v, v_fit, a_fit)
                 residuals = a_first - a_fit
@@ -125,16 +155,17 @@ def main():
                 self.output_console.append(f"❌ First spectrum fit error: {e}")
     
         def fit_last_spectrum(self):
-            if self.full_transient_df is None:
+            if LoadData.loaded_spectra is None:
                 self.output_console.append("⚠️ No transient data loaded.")
                 return
-            number_of_spectra = len(self.full_transient_df.iloc[0,2:])
-    
-            a_last = self.full_transient_df.iloc[:, -1].values
+            wavenumbers = LoadData.loaded_wavenumbers
+            a_last = LoadData.loaded_last_spectrum
+            number_of_spectra = LoadData.loaded_number_of_spectra
+
             centers = self.get_centers()
             k_max = int(self.kmax_input.text())
             try:
-                popt, _, v_fit, a_fit, comps = iterative_pekaria_fit(self.v, a_last, centers, k_max=k_max)
+                popt, _, v_fit, a_fit, comps = iterative_pekaria_fit(wavenumbers, a_last, centers, k_max=k_max)
                 fitted_centers = [popt[i * 6 + 2] for i in range(len(centers))]
                 #residuals = a_last - np.interp(self.v, v_fit, a_fit)
                 residuals = a_last - a_fit
@@ -159,8 +190,10 @@ def main():
                 self.output_console.append("⚠️ No last fit available to store.")
     
         def plot_fit_step(self, v_fit, a_fit, comps, centers, fitted_centers, residuals, index, total, a_exp):
+            wavenumbers = LoadData.loaded_wavenumbers
+
             self.ax_fit.clear()
-            self.ax_fit.plot(self.v, a_exp, label="Experimental", color='black')
+            self.ax_fit.plot(wavenumbers, a_exp, label="Experimental", color='black')
             for i, comp in enumerate(comps[:-1]):
                 label = f"PF{i+1}: guess={centers[i]:.0f}, fit={fitted_centers[i]:.0f}"
                 self.ax_fit.plot(v_fit, comp, '--', label=label)
@@ -172,24 +205,27 @@ def main():
             self.canvas_fit.draw()
     
             self.ax_res.clear()
-            self.ax_res.plot(self.v, residuals, color='red')
+            self.ax_res.plot(wavenumbers, residuals, color='red')
             self.ax_res.axhline(0, color='black', linestyle='--')
             self.ax_res.set_title("Residuals")
             self.ax_res.invert_xaxis()
             self.canvas_res.draw()
-    
-        
         
         def fit_all_spectra_with_weights(self):
+            ''' 
+            The previously obtained fits of the first and last spectra are used to obtain "fits" of all spectra by
+            combining the first and last spectra in a certain ratio (or weight).
+            '''
             if not hasattr(self, 'temp_fit_first') or not hasattr(self, 'temp_fit_last'):
                 self.output_console.append("❌ First and last fits must be available.")
                 return
-            if self.full_transient_df is None:
+            if LoadData.loaded_spectra is None:
                 self.output_console.append("❌ No full transient dataset loaded.")
                 return
     
-            spectra = self.full_transient_df.iloc[:, 2:].values.T  # shape: [n_spectra, n_points]
-            v = self.v
+            # spectra = LoadData.loaded_spectra.iloc[:, 2:].values.T  # shape: [n_spectra, n_points]
+            spectra = LoadData.loaded_spectra.T  # shape: [n_spectra, n_points]
+            wavenumbers = LoadData.loaded_wavenumbers
     
             fit_first = self.temp_fit_first
             fit_last = self.temp_fit_last
@@ -216,14 +252,14 @@ def main():
                     self.output_console.append(f"✅ Spectrum {i+1}: w = {w_opt:.3f}")
                 except Exception as e:
                     self.output_console.append(f"❌ Error on spectrum {i+1}: {e}")
-                    fitted_matrix.append(np.full_like(v, np.nan))
-                    residual_matrix.append(np.full_like(v, np.nan))
+                    fitted_matrix.append(np.full_like(wavenumbers, np.nan))
+                    residual_matrix.append(np.full_like(wavenumbers, np.nan))
     
             fitted_matrix = np.array(fitted_matrix).T  # [n_points, n_spectra]
             residual_matrix = np.array(residual_matrix).T  # [n_points, n_spectra]
     
-            df_fit = pd.DataFrame({'Wavenumber': self.v})
-            df_resid = pd.DataFrame({'Wavenumber': self.v})
+            df_fit = pd.DataFrame({'Wavenumber': wavenumbers})
+            df_resid = pd.DataFrame({'Wavenumber': wavenumbers})
     
             for i in range(fitted_matrix.shape[1]):
                 df_fit[f'Fit_{i+1}'] = fitted_matrix[:, i]
@@ -251,23 +287,26 @@ def main():
             if not hasattr(self, 'temp_fit_first') or not hasattr(self, 'temp_fit_last'):
                 self.output_console.append("❌ Both first and last fits are required to save.")
                 return
+            
+            wavenumbers = LoadData.loaded_wavenumbers
+
             path, _ = QFileDialog.getSaveFileName(self, "Save Fits", "", "CSV Files (*.csv)")
             if path:
                 df = pd.DataFrame({
-                    'Wavenumber': self.v,
+                    'Wavenumber': wavenumbers,
                     'Fit_First': self.temp_fit_first,
                     'Fit_Last': self.temp_fit_last
                 })
                 df.to_csv(path, index=False)
                 self.output_console.append(f"💾 First and last fits saved to {path}")            
-                #self.output_console.append("❌ First and last fits must be available.")
                 return
-            if self.full_transient_df is None:
+            if LoadData.loaded_spectra is None:
                 self.output_console.append("❌ No full transient dataset loaded.")
                 return
     
-            spectra = self.full_transient_df.iloc[:, 2:].values
-            v = self.v
+            # spectra = LoadData.loaded_spectra.iloc[:, 2:].values
+            spectra = LoadData.loaded_spectra
+            v = wavenumbers
             fit_first = np.interp(v, v, self.temp_fit_first)
             fit_last = np.interp(v, v, self.temp_fit_last)
     
@@ -305,27 +344,9 @@ def main():
                 self.output_console.append(f"💾 Weighted fits saved to {path}")
     #########################################################
     app = QApplication.instance() or QApplication(sys.argv)
-    filename = sys.argv[1]
-    df = pd.read_csv(filename, sep='\t')
-    v_trans = df.iloc[:, 1].values
-    spectra = df.iloc[:, 2:].values
-    a_trans = spectra[:, 0]  # First spectrum
-    ex = PekariaFitApp(v_trans, a_trans)
-    ex.first_spectrum_loaded_from_transient = True
-    ex.full_transient_df = df
+    ex = PekariaFitApp()
     ex.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    # app = QApplication.instance() or QApplication(sys.argv)
-    # filename = sys.argv[1]
-    # df = pd.read_csv(filename, sep='\t')
-    # v_trans = df.iloc[:, 1].values
-    # spectra = df.iloc[:, 2:].values
-    # a_trans = spectra[:, 0]  # First spectrum
-    # ex = PekariaFitApp(v_trans, a_trans)
-    # ex.first_spectrum_loaded_from_transient = True
-    # ex.full_transient_df = df
-    # ex.show()
-    # sys.exit(app.exec_())
     main()
